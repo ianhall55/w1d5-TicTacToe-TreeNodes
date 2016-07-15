b51388047a91d6a186a64748fc41c31b7b540c68
App Academy Student <student@appacademy.io>
2016-07-15 15:20:18 -0700

    Finished tic_tac_toe_node class

--- tic_tac_toe_ai/skeleton/lib/tic_tac_toe_node.rb
+++ tic_tac_toe_ai/skeleton/lib/tic_tac_toe_node.rb
@@ -11,26 +11,37 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
+    return false if @board.over? && (@board.winner == evaluator || @board.winner == nil)
+    return true if @board.over? && !(@board.winner == evaluator)
+
+    self.children.any? { |child| child.losing_node?(evaluator) }
   end
 
   def winning_node?(evaluator)
+    return true if @board.over? && @board.winner == evaluator
+    return false if @board.over? && !(@board.winner == evaluator)
+
+    self.children.any? { |child| child.winning_node?(evaluator) }
   end
 
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
     empty_pos = []
-    @board.each_with_index do |row, idx|
+    @board.rows.each_with_index do |row, idx|
       row.each_index do |col|
         pos = [idx, col]
         empty_pos << pos if @board.empty?(pos)
       end
     end
 
+    next_mover = (@next_mover_mark == :x) ? :o : :x
+
     empty_pos.map do |pos|
       new_board = @board.dup
       new_board[pos] = next_mover_mark
-      next_mover_mark = (next_mover_mark == :x) ? :o : :x
-      TicTacToeNode.new(new_board, next_mover_mark)
+
+      TicTacToeNode.new(new_board, next_mover, pos)
+    end
   end
 end
